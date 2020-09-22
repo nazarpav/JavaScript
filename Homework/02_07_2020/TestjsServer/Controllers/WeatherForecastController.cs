5447a8f421e123d3d74a9a56d2f35f47dcc6249b
Nazar <hesab.k18@gmail.com>
2020-09-22 21:34:14 +0300

    homework

---
+++ Homework/02_07_2020/TestjsServer/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TestjsServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserProfileController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<UserProfileController> _logger;
+
+        public UserProfileController(ILogger<UserProfileController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [EnableCors("MyPolicy")]
+        public UserProfile Get()
+        {
+            return new UserProfile { Id = 1, Name = "Rober", Surname = "Rebor", Email = "robero@gmail.com" };
+        }
+    }
+}
---
+++ Homework/02_07_2020/TestjsServer/UserProfile.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TestjsServer
+{
+    public class UserProfile
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+    }
+}
